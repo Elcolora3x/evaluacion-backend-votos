6a8ab0dd05d67885e2a7dd491c9099ff38ca3a1c
Elcolora3x <elcolora2x@gmail.com>
2025-10-24 17:08:38 -0300

    feat: add repository "PartidoPoliticoRepository"

---
+++ src/main/java/com/evaluacion/votos/evaluacion_backend_votos/repositories/PartidoPoliticoRepository.java
@@ -0,0 +1,8 @@
+package com.evaluacion.votos.evaluacion_backend_votos.repositories;
+
+import com.evaluacion.votos.evaluacion_backend_votos.models.PartidoPolitico;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PartidoPoliticoRepository extends JpaRepository<PartidoPolitico, Long> {
+
+}
