8d60420144267d1f2679d42762b54e886bafaa83
Elcolora3x <elcolora2x@gmail.com>
2025-10-27 04:18:07 -0300

    feat: add service class "VotoService"

---
+++ src/main/java/com/evaluacion/votos/evaluacion_backend_votos/services/VotoService.java
@@ -0,0 +1,39 @@
+package com.evaluacion.votos.evaluacion_backend_votos.services;
+
+import com.evaluacion.votos.evaluacion_backend_votos.models.Candidato;
+import com.evaluacion.votos.evaluacion_backend_votos.models.PartidoPolitico;
+import com.evaluacion.votos.evaluacion_backend_votos.models.Voto;
+import com.evaluacion.votos.evaluacion_backend_votos.repositories.VotoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class VotoService {
+    private VotoRepository repository;
+
+
+    @Autowired
+    public VotoService(VotoRepository repository, CandidatoService candidatoService) {
+        this.repository = repository;
+    }
+
+    public Voto createVoto(Voto voto) { return repository.save(voto); }
+
+    public Integer getVotosByCandidato(Candidato candidato){
+        List<Voto> votos = repository.findByCandidato(candidato);
+        return votos.size();
+    }
+
+    public Integer getVotosByPartidoPolitico(PartidoPolitico partidoPolitico){
+        Integer cantidadVotos = 0;
+        List<Candidato> candidatosDelPartido = CandidatoService.getCandidatosByPartidoPolitico(partidoPolitico);
+        for (Candidato candidato : candidatosDelPartido) {
+            cantidadVotos += getVotosByCandidato(candidato);
+        }
+
+        return cantidadVotos;
+    }
+}
