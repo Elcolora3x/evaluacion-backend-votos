343aa89d49bc42b9e059e53cf5985c08ff8a5b32
Elcolora3x <elcolora2x@gmail.com>
2025-10-27 05:13:23 -0300

    refactor: Erase unused imports

--- src/main/java/com/evaluacion/votos/evaluacion_backend_votos/services/CandidatoService.java
+++ src/main/java/com/evaluacion/votos/evaluacion_backend_votos/services/CandidatoService.java
@@ -1,14 +1,12 @@
 package com.evaluacion.votos.evaluacion_backend_votos.services;
 
 import com.evaluacion.votos.evaluacion_backend_votos.dtos.CandidatoDTO;
-import com.evaluacion.votos.evaluacion_backend_votos.dtos.PartidoPoliticoDTO;
 import com.evaluacion.votos.evaluacion_backend_votos.models.Candidato;
 import com.evaluacion.votos.evaluacion_backend_votos.models.PartidoPolitico;
 import com.evaluacion.votos.evaluacion_backend_votos.repositories.CandidatoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.awt.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.NoSuchElementException;
