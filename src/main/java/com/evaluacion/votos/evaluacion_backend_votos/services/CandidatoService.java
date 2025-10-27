f5fa61b148f7c5b91a406a6a2609755a07384cc4
Elcolora3x <elcolora2x@gmail.com>
2025-10-27 03:09:49 -0300

    feat: add service class "CandidatoService"

---
+++ src/main/java/com/evaluacion/votos/evaluacion_backend_votos/services/CandidatoService.java
@@ -0,0 +1,60 @@
+package com.evaluacion.votos.evaluacion_backend_votos.services;
+
+import com.evaluacion.votos.evaluacion_backend_votos.dtos.CandidatoDTO;
+import com.evaluacion.votos.evaluacion_backend_votos.dtos.PartidoPoliticoDTO;
+import com.evaluacion.votos.evaluacion_backend_votos.models.Candidato;
+import com.evaluacion.votos.evaluacion_backend_votos.models.PartidoPolitico;
+import com.evaluacion.votos.evaluacion_backend_votos.repositories.CandidatoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
+@Service
+public class CandidatoService {
+    private CandidatoRepository repository;
+
+    @Autowired
+    public CandidatoService(CandidatoRepository candidatoRepository) {
+        this.repository = candidatoRepository;
+    }
+
+    public List<CandidatoDTO> getCandidatos(){
+        List<Candidato> candidatos = repository.findAll();
+        List<CandidatoDTO> candidatosDTO = new ArrayList<>();
+
+        for (Candidato candidato : candidatos) {
+            CandidatoDTO candidatoDTO = new CandidatoDTO(candidato.getId(), candidato.getNombre(), candidato.getPartidoPolitico());
+            candidatosDTO.add(candidatoDTO);
+        }
+
+        return candidatosDTO;
+    }
+
+    public Candidato createCandidato(Candidato candidato){
+        return repository.save(candidato);
+    }
+
+    public Candidato updateCandidato(Long id, Candidato candidatoActualizado) throws NoSuchElementException {
+        Optional<Candidato> candidato = repository.findById(id);
+        if(candidato.isPresent()){
+            return repository.save(candidatoActualizado);
+        }
+        else{
+            throw new NoSuchElementException("No se encontro el candidato");
+        }
+    }
+
+    public void deleteCandidato(Long id) throws NoSuchElementException {
+        Optional<Candidato> candidato = repository.findById(id);
+        if(candidato.isPresent()){
+            repository.delete(candidato.get());
+        }
+        else{
+            throw new NoSuchElementException("No se encontro el candidato");
+        }
+    }
+}
