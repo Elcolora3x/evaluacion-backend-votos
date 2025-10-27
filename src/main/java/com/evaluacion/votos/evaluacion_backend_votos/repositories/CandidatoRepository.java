12797fbdf17d0b5e1046355c9d41262ae11818a5
Elcolora3x <elcolora2x@gmail.com>
2025-10-27 04:10:50 -0300

    feat: add method "findByPartidoPolitico(PartidoPolitico)"

--- src/main/java/com/evaluacion/votos/evaluacion_backend_votos/repositories/CandidatoRepository.java
+++ src/main/java/com/evaluacion/votos/evaluacion_backend_votos/repositories/CandidatoRepository.java
@@ -1,10 +1,14 @@
 package com.evaluacion.votos.evaluacion_backend_votos.repositories;
 
 import com.evaluacion.votos.evaluacion_backend_votos.models.Candidato;
+import com.evaluacion.votos.evaluacion_backend_votos.models.PartidoPolitico;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface CandidatoRepository extends JpaRepository<Candidato, Long> {
 
+    List<Candidato> findByPartidoPolitico(PartidoPolitico partidoPolitico);
 }
