ddb70f066162a1d676aa604148511ee2291ac60c
Elcolora3x <elcolora2x@gmail.com>
2025-10-30 18:34:44 -0300

    refactor: add test for all methods in service class

--- src/test/java/com/evaluacion/votos/evaluacion_backend_votos/services/PartidoPoliticoServiceTest.java
+++ src/test/java/com/evaluacion/votos/evaluacion_backend_votos/services/PartidoPoliticoServiceTest.java
@@ -17,8 +17,7 @@ import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class PartidoPoliticoServiceTest {
@@ -34,6 +33,7 @@ class PartidoPoliticoServiceTest {
 
     @BeforeEach
     void setUp() {
+
         partido1 = new PartidoPolitico();
         partido1.setId(1L);
         partido1.setNombre("Partido 1");
@@ -85,11 +85,124 @@ class PartidoPoliticoServiceTest {
             throws PartidoNoEncontradoException {
 
         Long idPartido = 1L;
-        List<PartidoPolitico> listaPartidos = List.of(partido1);
 
         given(partidoPoliticoRepository.findById(idPartido)).willReturn(Optional.ofNullable(partido1));
 
-        PartidoPoliticoDTO resultado = partidoPoliticoService.getPartidoById(idPartido);
+        PartidoPolitico resultado = partidoPoliticoService.getPartidoById(idPartido);
 
+        assertAll(
+    () -> assertNotNull(resultado),
+                () -> assertEquals(partido1, resultado)
+        );
+
+        verify(partidoPoliticoRepository, times(1)).findById(idPartido);
+    }
+
+    @Test
+    @DisplayName("Test de exepcion PartidoNoEncontradoException en metodo GetPartidoById")
+    void  testGetPartidoByIdNotFound()
+            throws PartidoNoEncontradoException {
+
+        Long idPartidoInexistente = 99L;
+        String mensajeExcepcion = "El partido político con id 99 no se encuentra en la base de datos.";
+
+        given(partidoPoliticoRepository.findById(idPartidoInexistente)).willReturn(Optional.empty());
+
+        PartidoNoEncontradoException excepcion = assertThrows(
+                PartidoNoEncontradoException.class,
+                () -> partidoPoliticoService.getPartidoById(idPartidoInexistente)
+        );
+
+        assertEquals(mensajeExcepcion, excepcion.getMessage());
+    }
+
+    @Test
+    @DisplayName("Test de creacion de partido")
+    void testCreatePartidoSuccess(){
+
+        given(partidoPoliticoRepository.save(partido1)).willReturn(partido1);
+
+        PartidoPolitico resultado =  partidoPoliticoService.createPartido(partido1);
+
+        assertAll(
+                () -> assertNotNull(resultado),
+                () -> assertEquals(partido1, resultado)
+        );
+
+        verify(partidoPoliticoRepository, times(1)).save(partido1);
+    }
+
+    @Test
+    @DisplayName("Test de actualizacion de partido")
+    void testUpdatePartidoSuccess()
+            throws PartidoNoEncontradoException {
+
+        Long idPartido = 1L;
+
+        PartidoPolitico partidoActualizado = new PartidoPolitico(idPartido, "Partido Actualizado", "PA")
+                    ;
+        given(partidoPoliticoRepository.findById(idPartido)).willReturn(Optional.ofNullable(partido1));
+        given(partidoPoliticoRepository.save(partido1)).willReturn(partido1);
+
+        PartidoPolitico resultado = partidoPoliticoService.updatePartido(idPartido, partidoActualizado);
+
+        assertAll(
+                () -> assertNotNull(resultado),
+                () -> assertEquals(partidoActualizado, resultado)
+        );
+
+        verify(partidoPoliticoRepository, times(1)).save(partido1);
+    }
+
+    @Test
+    @DisplayName("Test de excepcion PartidoNoEncontradoException en metodo UpdatePartido")
+    void testUpdatePartidoNotFound()
+            throws PartidoNoEncontradoException {
+
+        Long idPartidoInexistente = 99L;
+        String mensajeExcepcion = "El partido político con id 99 no se encuentra en la base de datos.";
+
+        given(partidoPoliticoRepository.findById(idPartidoInexistente)).willReturn(Optional.empty());
+
+        PartidoNoEncontradoException excepcion = assertThrows(
+                PartidoNoEncontradoException.class,
+                () -> partidoPoliticoService.updatePartido(idPartidoInexistente, any(PartidoPolitico.class))
+        );
+
+        assertEquals(mensajeExcepcion, excepcion.getMessage());
+        verify(partidoPoliticoRepository, never()).save(any(PartidoPolitico.class));
+    }
+
+    @Test
+    @DisplayName("Test de eliminacion de partido")
+    void testDeletePartidoSuccess()
+            throws PartidoNoEncontradoException {
+
+        Long idPartido = 1L;
+
+        given(partidoPoliticoRepository.findById(idPartido)).willReturn(Optional.ofNullable(partido1));
+
+        partidoPoliticoService.deletePartido(idPartido);
+
+        verify(partidoPoliticoRepository, times(1)).findById(idPartido);
+        verify(partidoPoliticoRepository, times(1)).delete(partido1);
+    }
+
+    @Test
+    @DisplayName("Test de excepcion PartidoNoEncontradoException para metodo DeletePartido")
+    void testDeletePartidoNotFound()
+            throws PartidoNoEncontradoException {
+
+        Long idPartidoInexistente = 99L;
+        String mensajeExcepcion = "El partido político con id 99 no se encuentra en la base de datos.";
+
+        given(partidoPoliticoRepository.findById(idPartidoInexistente)).willReturn(Optional.empty());
+
+        PartidoNoEncontradoException excepcion = assertThrows(
+                PartidoNoEncontradoException.class,
+                () -> partidoPoliticoService.deletePartido(idPartidoInexistente)
+        );
+
+        assertEquals(mensajeExcepcion, excepcion.getMessage());
     }
 }
